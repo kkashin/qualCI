0482975211a05de57379b6ae136b8a41a2b03c12
Konstantin Kashin <kkashin@fas.harvard.edu>
2014-06-30 10:14:45 -0400

    Added test.R

---
+++ demo/test.R
@@ -0,0 +1,25 @@
+library(qualci)
+data(plurality)
+
+
+##### PAIRS #####
+between.ranks <- c(3,4,2,1)
+dat <- prepareData(data=pluralityPairs, set="set", unit="country", treatment="treat", withinRank="rank", betweenRank=between.ranks)
+dat
+qout <- quade(dat)
+qout
+head(qout$permutations)
+qualci(qout)
+
+
+##### SETS #####
+between.ranks <- c(2,3,1)
+dat <- prepareData(data=pluralitySets, set="set", unit="country", treatment="treat", withinRank="rank", betweenRank=between.ranks)
+qout <- quade(dat)
+dat
+qout
+head(qout$permutations)
+qualci(qout)
+
+
+sapply(qout$sets, function(st) st$obsTreat==1)
