f7e174693fa795d6dad87e8ec9c9191dc6d92fa7
adam-glynn <aglynn@Adams-MacBook-Pro-3.local>
2014-07-02 14:53:29 -0400

    minor test update

--- demo/test.R
+++ demo/test.R
@@ -5,7 +5,7 @@ data(pluralitySets)
 
 ##### PAIRS #####
 between.ranks <- c(3,4,2,1)
-dat <- prepareData(data=pluralityPairs, set="pair", unit="country", treatment="plurality", withinRank="OppHarRankSet", betweenRank=between.ranks)
+dat <- prepareData(data=pluralityPairs, set="pair", unit="country", treatment="plurality", withinRank="OppHarRank", betweenRank=between.ranks)
 dat
 qout <- quade(dat)
 qout
@@ -15,7 +15,7 @@ qualci(qout)
 
 ##### SETS #####
 between.ranks <- c(2,3,1)
-dat <- prepareData(data=pluralitySets, set="set", unit="country", treatment="plurality", withinRank="OppHarRankSet", betweenRank=between.ranks)
+dat <- prepareData(data=pluralitySets, set="set", unit="country", treatment="plurality", withinRank="OppHarRank", betweenRank=between.ranks)
 qout <- quade(dat)
 dat
 qout
